7dfeaa40cec3b2fc961f95ee20f678ad98e5f68f
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2023-02-02 06:37:30 -0800

    Key change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Main{
     public static void main(String[] args) throws Exception {
          JDA jda = JDABuilder
-                .createDefault("MTA2NjUxNTM4NTI2MTc3MjgwMA.GobII2.jUyaSk8KYIKfzVo744Lja0Ike5PZ90sG2bGnis").enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .createDefault("MTA2NjUxNTM4NTI2MTc3MjgwMA.GHdzFF.66A9Z6GtwWG-8yqTnopGuEpSRN51pU2GvtBEx0").enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build();
         jda.addEventListener(new MyListener());
 
